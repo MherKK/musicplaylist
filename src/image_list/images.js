69c9e3ae508f8c406bf49240e19f71fc940c80ff
MherKK <mherkevorkian2@gmail.com>
2024-01-23 22:50:50 +0400

    corrected the assets's name

--- src/image_list/images.js
+++ src/image_list/images.js
@@ -6,7 +6,7 @@ import share from '../assets/share.png'
 import shuffle from '../assets/shuffle.png'
 import tick from '../assets/tick.png'
 import search from '../assets/search.png'
-import redHeart from '../assets/redHeart.png'
+import redHeart from '../assets/redheart.png'
 import upArrow from '../assets/uparrow.png'
 import downArrow from '../assets/downarrow.png'
 //importing all images in one place to have an easier access and more organized code
